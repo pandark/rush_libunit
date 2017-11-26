7e5d849523575d6149327930b0ed553fa66808c3
Adrien PACHKOFF <apachkof@student.42.fr>
2017-11-26 22:04:56 +0100

    dummy test banner

--- tests/dummy/00_launcher.c
+++ tests/dummy/00_launcher.c
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 23:03:33 by apachkof          #+#    #+#             */
-/*   Updated: 2017/11/26 21:22:01 by apachkof         ###   ########.fr       */
+/*   Updated: 2017/11/26 22:04:09 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int	dummy_launcher(void)
 {
 	t_unit_test *testlist;
 
-	write(2, "DUMMY:\n", sizeof("DUMMY:\n"));
+	write(2, "\033[36m===============[ DUMMY ]==============>\033[0m\n",
+	sizeof("\033[36m===============[ DUMMY ]==============>\033[0m\n"));
 	load_test(&testlist, "Always true test", 0, &always_true);
 	load_test(&testlist, "Always false test", 0, &always_false);
 	load_test(&testlist, "Always segfault test", 0, &always_segv);
